b548de0629a2488bf33e44f5ba8e9291afad48e7
kelseykcm <kcm_unix@yahoo.com.br>
2024-01-28 18:03:31 -0300

    subir Site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { NavbarComponent } from "./sharepage/navbar/navbar.component";
+import { FooterComponent } from "./sharepage/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'LabsSecurity';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes, RoutesRecognized } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CategoriaComponent } from './pages/categoria/categoria.component';
+import path from 'path';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'categoria',component:CategoriaComponent},
+  {path:'sobre',component:SobreComponent},
+  {path:'contato',component:ContatoComponent},
+  {path: '**', component: PagenotfoundComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
